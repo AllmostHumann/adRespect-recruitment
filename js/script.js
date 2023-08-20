b1782d3156e865d0d5897e2b1f6349d2e0813d8c
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-08-20 18:20:04 +0200

    sections added, masonry cdn added, script added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const grid = document.querySelector(".grid");
+
+const msnry = new Masonry(grid, {
+  itemSelector: ".grid-item",
+  horizontalOrder: true,
+  fitWidth: true,
+  gutter: 43,
+});
